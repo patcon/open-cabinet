b5f31145681b48483f9a5726d2cc9571a617a83e
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-01-11 19:01:09 -0800

    Simpler port setting.

--- openorders/__init__.py
+++ openorders/__init__.py
@@ -64,8 +64,5 @@ def search():
 
 
 if __name__ == "__main__":
-    port = os.environ.get('PORT')
-    if port:
-        app.run(port=int(port))
-    else:
-        app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
